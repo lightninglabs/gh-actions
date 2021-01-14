911d55aa9c099e534f6ff949959f6ec723edfa02
Oliver Gugger <gugger@gmail.com>
2021-01-14 11:06:32 +0100

    aws: remove amazon-ecr-login and configure-aws-credentials
    Turns out that the docker/login-action can also log into AWS ECR with
    the pre-installed aws binary on the GitHub runner VM.
    So we can remove two actions which greatly reduces the amount of code
    to review.

--- vendor.sh
+++ vendor.sh
@@ -27,5 +27,3 @@ function vendor() {
 
 vendor "softprops" "action-gh-release" "91409e712cf565ce9eff10c87a8d1b11b81757ae" "build"
 vendor "docker" "login-action" "f3364599c6aa293cdc2b8391b1b56d0c30e45c8a" "build"
-vendor "aws-actions" "configure-aws-credentials" "e97d7fbc8e0e5af69631c13daa0f4b5a8d88165b" "package"
-vendor "aws-actions" "amazon-ecr-login" "b9c809dc38d74cd0fde3c13cc4fe4ac72ebecdae" "package"
